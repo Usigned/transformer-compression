07430c36339460bcf182db60ad48d08b4c2312e9
qing <843710843@qq.com>
2023-04-19 23:16:53 +0800

    create 3 files and update 2 files

---
+++ mmsa.py
@@ -0,0 +1,257 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from model import LinearGeneral, CAFIA_Transformer
+from utils import freeze_but_type, assert_model_all_freezed, assert_all_module_type_freezed, set_freeze_all
+import os
+
+class LearnableMask(nn.Module):
+    def __init__(self, dim, prune_rate=1.) -> None:
+        super().__init__()
+        self.dim = dim
+        self.pruned_dim = 0
+        self.mask = nn.Parameter(torch.ones(dim))
+        self.prune_rate = prune_rate
+        self._mask = torch.ones_like(self.mask, requires_grad=False)
+        self.is_fixed = False
+        self.random_mask = False
+
+    def forward(self, x):
+        mask = self.get_mask()
+        x = torch.matmul(x, torch.diag(mask))
+        return x, mask
+
+    def get_mask(self):
+        if self.random_mask:
+            return self._mask
+        mask = self.mask if self.is_fixed else self.prune_rate * self.dim * torch.softmax(self.mask, 0)
+        return torch.mul(mask, self._mask)
+
+    def fix_mask(self, ratio=1.0):
+        self.mask.requires_grad = False
+        self.prune_rate = ratio
+        self.mask = nn.Parameter(self.get_mask())
+        self.is_fixed = True
+        return self.mask
+    
+    def make_prune_mask(self, prune_rate):
+        self.random_mask = False
+        if not self.is_fixed:
+            self.fix_mask()
+        _mask = torch.ones_like(self._mask, requires_grad=False)
+        self.pruned_dim = self.dim - int(prune_rate*self.dim)
+        _, idxs = torch.sort(self.mask)
+
+        for idx in idxs[:self.pruned_dim]:
+            _mask[idx] = 0
+        self._mask = _mask
+
+    def make_random_prune_mask(self, prune_rate):
+        self.random_mask = True
+        self.pruned_dim = self.dim - int(prune_rate*self.dim)
+        _mask = torch.cat((torch.ones(int(prune_rate*self.dim)), torch.zeros(self.pruned_dim)), 0)
+        self._mask = nn.Parameter(_mask[torch.randperm(self.dim)])
+
+
+def fix_dmask(mvit:nn.Module):
+    for m in mvit.modules():
+        if type(m) is LearnableMask:
+            m._mask.requires_grad = False
+
+def to_mask(mvit:nn.Module, device):
+    for m in mvit.modules():
+        if type(m) is LearnableMask:
+            m._mask = m._mask.to(device)
+
+def fix_mask(model:CAFIA_Transformer, ratio=1.):
+    for m in model.modules():
+        if type(m) is LearnableMask:
+            m.fix_mask(ratio)
+
+def prune(model:CAFIA_Transformer, prune_rate=1., random=False):
+    # fix_mask(model)
+    set_mask_prune_rate(model, prune_rate) #只是为了展示，不会影响推理
+    for m in model.modules():
+        if type(m) is LearnableMask:
+            if random:
+                m.make_random_prune_mask(prune_rate)
+            else:
+                m.make_prune_mask(prune_rate)
+
+def set_mask_prune_rate(model:CAFIA_Transformer, prune_rate):
+    for m in model.modules():
+        if type(m) is LearnableMask:
+            m.prune_rate=prune_rate
+
+class MaskedSelfAttention(nn.Module):
+    def __init__(self, in_dim, heads=8, dropout_rate=0.1, prune_rate=.25):
+        super(MaskedSelfAttention, self).__init__()
+        self.heads = heads
+        self.head_dim = in_dim // heads
+        self.scale = self.head_dim ** 0.5
+        self.prune_rate = prune_rate
+
+        self.query = LinearGeneral((in_dim,), (self.heads, self.head_dim))
+        self.key = LinearGeneral((in_dim,), (self.heads, self.head_dim))
+        self.value = LinearGeneral((in_dim,), (self.heads, self.head_dim))
+        self.out = LinearGeneral((self.heads, self.head_dim), (in_dim,))
+
+        self.mask = LearnableMask(self.heads, self.prune_rate)
+
+        if dropout_rate > 0:
+            self.dropout = nn.Dropout(dropout_rate)
+        else:
+            self.dropout = None
+
+    def forward(self, x, need_mask=False):
+        b, n, _ = x.shape
+
+        q = self.query(x, dims=([2], [0]))  # b,n,heads,head_dim
+        k = self.key(x, dims=([2], [0]))
+        v = self.value(x, dims=([2], [0]))
+
+        q = q.permute(0, 2, 1, 3)
+        k = k.permute(0, 2, 1, 3)
+        v = v.permute(0, 2, 1, 3)  # b,heads,n,head_dim
+
+        attn_weights = torch.matmul(q, k.transpose(-2, -1)) / self.scale
+        attn_weights = F.softmax(attn_weights, dim=-1)  # b,heads,n,n
+        out = torch.matmul(attn_weights, v)  # b, heads, n, head_dim
+        out = out.permute(0, 2, 3, 1)  # b, n, head_dim, heads
+        out, mask = self.mask(out)
+        out = out.permute(0, 1, 3, 2)  # b, n, heads, head_dim
+        out = self.out(out, dims=([2, 3], [0, 1]))
+
+        return (out, mask) if need_mask else out
+
+    def get_mask(self):
+        return self.mask.get_mask()
+
+
+def build_mvit(args):
+    args.attn_type = MaskedSelfAttention
+    args.vit_model = None
+    if not hasattr(args, 'pr'):
+        args.pr = 1.0
+    mvit = CAFIA_Transformer(args)
+    if hasattr(args, 'cifar10_vit'):
+        if hasattr(args, 'pwd'):
+            args.cifar10_vit = os.path.join(args.pwd, args.cifar10_vit)
+        mvit = load_weight_for_mvit(mvit, path=args.cifar10_vit)
+        print(f"load pretrained weight from {args.cifar10_vit}.")
+    else:
+        print(f"no pretrained weight loaded cause no path is specified.")
+    set_mask_prune_rate(mvit, args.pr)
+    return mvit
+
+
+def test_mmsa():
+    b, n, in_dim, heads = 64, 32, 888, 8
+    in_shape = (b, n, in_dim)
+    mmsa = MaskedSelfAttention(in_dim=in_dim, heads=heads)
+    out, mask = mmsa(torch.randn(*in_shape), need_mask=True)
+    print(mask)
+    assert out.shape == (b, n, in_dim), \
+        f'test failed expected out dim {in_shape}, got {out.shape}'
+
+
+def test_cafia_tfm():
+    import json
+    from argparse import Namespace
+    args = Namespace(
+        **json.load(open('Vision-Transformer-ViT/ViT-B_16-224.json', 'r')))
+    args.attn_type = MaskedSelfAttention
+    args.vit_model = None
+
+    vit = CAFIA_Transformer(args)
+
+    b, c, h, w = (args.batch_size, 3, args.image_size, args.image_size)
+
+    out = vit(torch.randn(*(b, c, h, w)))
+    assert out.shape == (b, args.num_classes), \
+        f'test failed expected out dim {(b, args.num_classes)}, got {out.shape}'
+
+
+def test_mvit_bp():
+    import json
+    from argparse import Namespace
+    args = Namespace(
+        **json.load(open('Vision-Transformer-ViT/ViT-B_16-224.json', 'r')))
+    args.attn_type = MaskedSelfAttention
+    args.vit_model = None
+
+    device = 'cuda:0'
+
+    vit = CAFIA_Transformer(args).to(device)
+
+    b, c, h, w = (args.batch_size, 3, args.image_size, args.image_size)
+
+    out = vit(torch.randn(*(b, c, h, w)).to(device))
+    label = torch.randn(b, args.num_classes).to(device)
+
+    loss = F.cross_entropy(out, label)
+    loss.backward()
+    print(loss)
+
+
+def get_masks_from_mmsa(module: nn.Module):
+    masks = []
+    for child in module.children():
+        if type(child) is LearnableMask:
+            masks += [child.mask]
+        else:
+            masks += get_masks_from_mmsa(child)
+    return masks
+
+
+def get_mask_val_from_masks(module: nn.Module):
+    '''
+    use get_mask() fn to get mask val during inference
+    '''
+    masks = []
+    for child in module.children():
+        if type(child) is LearnableMask:
+            masks += [child.get_mask()]
+        else:
+            masks += get_mask_val_from_masks(child)
+    return masks
+
+
+def freeze_model_but_mask(model: CAFIA_Transformer):
+    return freeze_but_type(model, LearnableMask)
+
+
+def _test_freeze():
+    import json
+    from argparse import Namespace
+    args = Namespace(
+        **json.load(open('Vision-Transformer-ViT/ViT-B_16-224.json', 'r')))
+    model = build_mvit(args)
+
+    # set_freeze_all(model, True)
+    freeze_model_but_mask(model)
+    assert_model_all_freezed(model, True, LearnableMask)
+    assert_all_module_type_freezed(model, LearnableMask, freeze=False)
+
+
+def load_weight_for_mvit(model: CAFIA_Transformer, path: str):
+    model.load_state_dict(torch.load(path, map_location=torch.device('cpu')), strict=False)
+    return model
+
+def show_dmask(mvit):
+    for m in mvit.modules():
+        if type(m) is LearnableMask:
+            print(m._mask)
+
+if __name__ == '__main__':
+    # test_mmsa()
+    # test_cafia_tfm()
+    # test_mvit_bp()
+    # _test_freeze()
+    pass
+    # lm = LearnableMask(dim=12)
+    # lm.mask = nn.Parameter(torch.randn(12))
+    # freeze_but_type(lm, LearnableMask)
+    # print(lm._mask.requires_grad)
+    # lm.make_random_prune_mask(0.5)
+    # lm(torch.randn(1, 12))
--- train.py
+++ train.py
@@ -10,29 +10,36 @@ from model import CAFIA_Transformer
 from tqdm import tqdm
 import logging
 from scheduler import cosine_lr
+import torch.nn.functional as F
+from mmsa import build_mvit, freeze_model_but_mask, get_mask_val_from_masks, set_mask_prune_rate, to_mask, fix_dmask
+from data import get_cifar10_dataloader
 
-logging.basicConfig(level = logging.NOTSET)
- 
+logging.basicConfig(level=logging.NOTSET)
+
+
 def train_model(args, trainloader, testloader, device='cpu'):
     n_gpu = 1
     model = CAFIA_Transformer(args)
     model.to(device)
-    
+
     if n_gpu > 1:
         model = torch.nn.DataParallel(model)
     model.train()
     loss_fn = nn.CrossEntropyLoss()
-    optimizer = optim.AdamW(model.parameters(), betas=(args.beta1, args.beta2), eps = args.eps, lr = args.learning_rate, weight_decay = args.weight_decay)
-    
-    total_steps = (len(trainloader) // args.batch_size + 1)  * args.epoches
-    scheduler = cosine_lr(optimizer, args.learning_rate, args.warmup, total_steps)   
+    optimizer = optim.AdamW(model.parameters(), betas=(
+        args.beta1, args.beta2), eps=args.eps, lr=args.learning_rate, weight_decay=args.weight_decay)
+
+    total_steps = (len(trainloader) // args.batch_size + 1) * args.epoches
+    scheduler = cosine_lr(optimizer, args.learning_rate,
+                          args.warmup, total_steps)
     nb_tr_steps = 0
-    logging.info('**************************** start to train *******************************')
+    logging.info(
+        '**************************** start to train *******************************')
     for epoch in range(args.epoches):
-        train_loss = 0 
+        train_loss = 0
         train_iter = 0
-        for _, batch in enumerate(tqdm(trainloader, desc = "Iteration")):
-            nb_tr_steps += 1  
+        for _, batch in enumerate(tqdm(trainloader, desc="Iteration")):
+            nb_tr_steps += 1
             optimizer.zero_grad()
             batch = tuple(t.to(device) for t in batch)
             batch_X, batch_Y = batch
@@ -44,23 +51,25 @@ def train_model(args, trainloader, testloader, device='cpu'):
             train_loss += loss.item()
             train_iter += 1
             logging.info('Epoch:%d batch_loss:%f', epoch, loss)
-        
+
         train_loss /= train_iter
 
-        #eval
-        logging.info('**************************** start to evaluate *******************************')
+        # eval
+        logging.info(
+            '**************************** start to evaluate *******************************')
         model.eval()
         total, correct = 0, 0
-        for _, batch in enumerate(tqdm(testloader, desc = "Iteration")):
+        for _, batch in enumerate(tqdm(testloader, desc="Iteration")):
             batch = tuple(t.to(device) for t in batch)
             batch_X, batch_Y = batch
             outputs = model(batch_X)
             _, predicted = torch.max(outputs.data, 1)
             total += batch_Y.size(0)
             correct += (predicted == batch_Y).sum()
-            
-        acc = (correct / total).item()
-        logging.info('Epoch: %d train_loss: %f Accuracy: %f', epoch, train_loss, acc)
+
+        acc = (correct / total).item()  # type: ignore
+        logging.info('Epoch: %d train_loss: %f Accuracy: %f',
+                     epoch, train_loss, acc)
         if not os.path.exists(args.output):
             os.mkdir(args.output)
         output_path = os.path.join(args.output, str(acc)+'.pt')
@@ -68,37 +77,39 @@ def train_model(args, trainloader, testloader, device='cpu'):
 
 
 def eval_model(model, dataloader, device='cpu'):
-    logging.info('**************************** start to evaluate *******************************')
+    logging.info(
+        '**************************** start to evaluate *******************************')
     model.eval()
     model = model.to(device)
     total, correct = 0, 0
-    for _, batch in enumerate(tqdm(dataloader, desc = "Iteration")):
+    for _, batch in enumerate(tqdm(dataloader, desc="Iteration")):
         batch = tuple(t.to(device) for t in batch)
         batch_X, batch_Y = batch
         outputs = model(batch_X)
         _, predicted = torch.max(outputs.data, 1)
         total += batch_Y.size(0)
         correct += (predicted == batch_Y).sum()
-        
-    acc = (correct / total).item()
+
+    acc = (correct / total).item()  # type: ignore
     logging.info('Accuracy: %f', acc)
     return acc
 
 
-def finetune(model:nn.Module, trainloader, device, epoches=1, save_dir=None, fname=None, **optim_kwargs):
-    
-    logging.info(f'**************************** start to finetune {epoches} turns*******************************')
+def finetune(model: nn.Module, trainloader, device, epoches=1, save_dir=None, fname=None, **optim_kwargs):
+
+    logging.info(
+        f'**************************** start to finetune {epoches} turns*******************************')
     model.train()
     model = model.to(device)
-    
+
     if 'lr' not in optim_kwargs:
         optim_kwargs['lr'] = 1e-4
-    
+
     optimizer = optim.Adam(model.parameters(), **optim_kwargs)
     loss_fn = nn.CrossEntropyLoss()
 
     for epoch in range(epoches):
-        for _, batch in enumerate(tqdm(trainloader, desc = "Iteration")):
+        for _, batch in enumerate(tqdm(trainloader, desc="Iteration")):
             optimizer.zero_grad()
             batch = tuple(t.to(device) for t in batch)
             batch_X, batch_Y = batch
@@ -112,4 +123,81 @@ def finetune(model:nn.Module, trainloader, device, epoches=1, save_dir=None, fna
         if not os.path.exists(save_dir):
             pass
         torch.save(model.state_dict(), os.path.join(save_dir, fname+'.pt'))
+    return model
+
+
+def train_mvit(args, trainloader=None, model=None, freeze_w=False, device='cpu', save_path=None, log_info=True):
+
+    def loss_fn(x, target, model, alpha=1e-2):
+        loss = F.cross_entropy(x, target)
+        # masks = get_masks_from_mmsa(model)
+        masks = get_mask_val_from_masks(model)
+        for mask in masks:
+            loss += torch.norm(mask, 2) * alpha  # type: ignore
+        return loss
+
+    if not model:
+        model = build_mvit(args)
+    model = model.to(device)
+    to_mask(model, device)
+
+    if freeze_w:
+        model = freeze_model_but_mask(model)
+
+    fix_dmask(model)
+
+    if not trainloader:
+        trainloader = get_cifar10_dataloader(root=os.path.join(
+            args.pwd, './data'), train=True, batch_size=args.batch_size)
+
+    model.train()
+
+    pr = args.pr if hasattr(args, 'pr') else 1.0
+    set_mask_prune_rate(model, pr)  # type: ignore
+
+    optimizer = optim.Adam(model.parameters(), lr=args.learning_rate)
+    alpha = args.alpha
+    # total_steps = (len(trainloader) // args.batch_size + 1)  * args.epoches
+    # scheduler = cosine_lr(optimizer, args.learning_rate, args.warmup, total_steps)
+    nb_tr_steps = 0
+
+    logging.info(
+        '**************************** start to train mvit *******************************')
+    for epoch in range(args.epoches):
+        train_loss = 0
+        train_iter = 0
+        for _, batch in enumerate(tqdm(trainloader, desc="Iteration")):
+            nb_tr_steps += 1
+            optimizer.zero_grad()
+            batch = tuple(t.to(device) for t in batch)
+            batch_X, batch_Y = batch
+            outputs = model(batch_X)
+            loss = loss_fn(outputs, batch_Y, model, alpha)
+            loss.backward()
+            # print_mask_grad()
+
+            if log_info:
+                pass
+                # mask = model.vit.transformer.encoder_layers[0].attn.mask.get_mask()
+                # writer.add_scalars('masks', {f'mask-{i}': mask[i] for i in range(len(mask))}, nb_tr_steps)
+                # stds = [torch.std(m) for m in get_mask_val_from_masks(model)]
+                # writer.add_scalars('stds', {f'std-{i}': stds[i] for i in range(len(stds))}, nb_tr_steps)
+
+            optimizer.step()
+            # scheduler(nb_tr_steps)
+            train_loss += loss.item()
+            train_iter += 1
+            # logging.info('Epoch:%d batch_loss:%f', epoch, loss)
+
+        train_loss = train_loss / train_iter
+
+        if epoch % 10 == 0:
+            dir_path = os.path.join(args.pwd, args.output, f'pr={pr}')
+            if not os.path.exists(dir_path):
+                os.makedirs(dir_path, exist_ok=True)
+            output_path = os.path.join(dir_path, str(
+                train_loss)+'.pt') if save_path is None else save_path
+            logging.info(
+                f'**************************** save model to {output_path} *******************************')
+            torch.save(model.state_dict(), output_path)
     return model
